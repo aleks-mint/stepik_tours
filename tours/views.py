1fea83b56762fb178db98ec3921997a3e3cb325b
aleks-mint <allessouth@gmail.com>
2022-04-04 22:18:43 +0800

    Create rep

---
+++ tours/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import render
+
+
+def custom_handler404(request, exception):
+    # Call when Http404 raised
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    # Call when raised some python exception
+    return HttpResponseServerError('Ошибка сервера!')
+
+
+def main_view(request):
+    return render(request, "index.html")
+
+
+def departure_view(request, departure):
+    return render(request, "departure.html")
+
+
+def tour_view(request, id):
+    return render(request, "tour.html")
+
+
+# def custom_handler404(request, exception):
+#     return HttpResponseNotFound('Ресурс не найден!')
+
+
+# def custom_handler500(request):
+#     return HttpResponseServerError('Ошибка сервера!')
